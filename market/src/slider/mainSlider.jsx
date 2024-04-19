78891c2b29c8e9ae20dc3db4bcf9c6653ff4e0e3
Seyran <mellodia163@gmail.com>
2024-04-19 21:59:28 +0400

    added slider

--- market/src/slider/mainSlider.jsx
+++ market/src/slider/mainSlider.jsx
@@ -1,25 +1,8 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import { Slider } from "./slider"
 
 const MainSlider = () => {
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            loop={true}
-            pagination={{ clickable: true }}
-
-        >
-            <SwiperSlide className='bg-lime-700'>Slide 1</SwiperSlide>
-            <SwiperSlide className='bg-lime-100'>Slide 2</SwiperSlide>
-            <SwiperSlide className='bg-lime-500'>Slide 3</SwiperSlide>
-            <SwiperSlide className='bg-lime-900'>Slide 4</SwiperSlide>
-        </Swiper>
+        <Slider></Slider>
     )
 }
 
